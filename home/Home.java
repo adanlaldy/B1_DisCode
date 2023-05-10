e3a7cade0d0593d85990f1b185d6423f89864e07
ThatDovesAway <rafael.lafon@ynov.com>
2023-05-10 15:16:02 +0200

    Set Up the Chat in Saloon:  -Using Expetions for keeping using the chat  -Fixing the bugs  -Fixing the intialisation of the classes in Home

--- home/Home.java
+++ home/Home.java
@@ -6,12 +6,76 @@ import servers.Server;
 import servers.Under_saloon;
 import user.User;
 
+import java.util.Objects;
 import java.util.Scanner;
+
 
 public class Home {
     private static boolean exit;
 
     public static void home() {
+
+
+        // à traduire
+        System.out.println("--Bienvenue sur DisCode, le discord dédié aux passionnés de codage informatique.\nUne fois connecté, vous pourrez intégrer des salons, triés par catégories, eux-mêmes situés sur un serveur pour pouvoir lire des informations ou en demander/partager.--\n");
+        // demande à l'utilisateur de rentrer son nom
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a name: ");
+        User user1 = new User(scanner.nextLine());
+        System.out.println("Welcome to inside of DisCode !!");
+        while (!exit) {
+            server_choice();
+        }
+        System.out.println("See you soon! :]");
+
+
+    }
+
+    private static void server_choice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("You can select a server between:\n(1) Web\n(2) POO\n(3) Application\n(4) Exit\nYour choice: ");
+        // gérer les exceptions lettres etc
+        int server_choice = Integer.parseInt(scanner.nextLine());
+        switch (server_choice) {
+            case 1:
+                // Exception, si (category_choice() == 3) -> break;
+                if (category_choice() == 1) {
+                    saloon_choice(1);
+                } else {
+                    saloon_choice(2);
+                }
+                break;
+            case 2:
+                // Exception, si (category_choice() == 3) -> break;
+                if (category_choice() == 1) {
+                    saloon_choice(3);
+                } else {
+                    saloon_choice(4);
+                }
+                break;
+            case 3:
+                // Exception, si (category_choice() == 3) -> break;
+                if (category_choice() == 1) {
+                    saloon_choice(5);
+                } else {
+                    saloon_choice(6);
+                }
+                break;
+            case 4:
+                exit = true;
+                break;
+        }
+    }
+
+    private static int category_choice() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("You can select a category:\n(1) Front-end\n(2) Back-end\n(3) Return\nYour choice: ");
+        // gérer les exceptions lettres etc
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+
+    private static void saloon_choice(int choice) {
         //----------SERVERS-------------------------
         Server web_server = new Server("Web");
         Server poo_server = new Server("POO");
@@ -81,68 +145,22 @@ public class Home {
 
         Under_saloon php_chatting = new Under_saloon("PHP-Chatting");
         Under_saloon php_faq = new Under_saloon("PHP-FAQ");
-
-
-        // à traduire
-        System.out.println("--Bienvenue sur DisCode, le discord dédié aux passionnés de codage informatique.\nUne fois connecté, vous pourrez intégrer des salons, triés par catégories, eux-mêmes situés sur un serveur pour pouvoir lire des informations ou en demander/partager.--\n");
-        // demande à l'utilisateur de rentrer son nom
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a name: ");
-        User user1 = new User(scanner.nextLine());
-        System.out.println("Welcome to inside of DisCode !!");
-        while (!exit) {
-            server_choice();
-        }
-        System.out.println("See you soon! :)");
-    }
-
-    private static void server_choice() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("You can select a server between:\n(1) Web\n(2) POO\n(3) Application\n(4) Exit\nYour choice: ");
-        // gérer les exceptions lettres etc
-        int server_choice = Integer.parseInt(scanner.nextLine());
-        switch (server_choice) {
-            case 1:
-                // Exception, si (category_choice() == 3) -> break;
-                if (category_choice() == 1) {
-                    saloon_choice(1);
-                } else {
-                    saloon_choice(2);
-                }
-                break;
-            case 2:
-                // Exception, si (category_choice() == 3) -> break;
-                if (category_choice() == 1) {
-                    saloon_choice(3);
-                } else {
-                    saloon_choice(4);
-                }
-                break;
-            case 3:
-                // Exception, si (category_choice() == 3) -> break;
-                if (category_choice() == 1) {
-                    saloon_choice(5);
-                } else {
-                    saloon_choice(6);
-                }
-                break;
-            case 4:
-                exit = true;
-                break;
-        }
-    }
-
-    private static int category_choice() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("You can select a category:\n(1) Front-end\n(2) Back-end\n(3) Return\nYour choice: ");
-        // gérer les exceptions lettres etc
-        return Integer.parseInt(scanner.nextLine());
-    }
-
-    private static void saloon_choice(int choice) {
         switch (choice) {
             case 1:
-                System.out.println("salon beginner, front (tous les langages simples en front)");
+                System.out.println("Wellcome to Front-End chat ! Enjoy !");
+                Scanner scanner = new Scanner(System.in);
+                while (!Objects.equals(scanner.nextLine(), "exit")){
+                    try {
+                        html_chatting.add_content(scanner.nextLine());
+                        html_chatting.print_content();
+                    } catch (NullPointerException e){
+
+                    }finally {
+                        System.out.print("");
+                    }
+
+                }
+
                 // accès au salon spécifique
                 // dans salon spécifique (appeler/configurer setExit)
                 break;
--- servers/Saloon.java
+++ servers/Saloon.java
@@ -5,18 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Saloon extends Category{
-    private String userText;
+
     private String name;
     private List<String> chatting;
     public Saloon(String name) {
         super(name);
         this.name = name;
     }
-    public void set_user_text(String content){
-        userText = content;
+
+    public void add_content(String content){
+        chatting.add(content);
     }
 
-    public void add_content(){
-        chatting.add(userText);
+    public void print_content(){
+        System.out.println(chatting);
     }
 }
